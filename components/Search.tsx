927e4d8a2bad7d2c0e4e29d9d2ccd2da72fc7559
Anthobad <anthonybadawi100@gmail.com>
2026-02-22 18:43:21 +0200

    enhance the searchbar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image";
 const Search = () => {
   return (
     <div className="h-10 gap-3 w-full max-w-xl border border-gray-300 rounded-2xl flex items-center px-4 bg-black mx:hidden">
-      <select name="search-category" id="search-category" className="bg-transparent outline-none text-xl">
+      <select name="search-category" id="search-category" className="bg-transparent outline-none text-xl w-24">
         <option value="all">All</option>
         <option value="games">Games</option>
         <option value="maps">Maps</option>
       </select>
-      <input type="text" placeholder="Search..." className="flex-1 bg-transparent outline-none text-sm" />
+      <Image src="/search-svgrepo-com.svg" alt="Search icon" width={20} height={20} className="filter invert"/>
+      <input type="text" placeholder="Search..." className="flex-1 bg-transparent outline-none text-xl" />
     </div>
   )
 }
