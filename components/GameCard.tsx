49daf52d56a1238c80999e02442c9ca84e87cdaf
Anthobad <anthonybadawi100@gmail.com>
2026-02-24 12:47:19 +0200

    make card clickable instead of open buttons taking useless space

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -32,24 +32,21 @@ const GameCard = ({ gameSlug, name, thumbnailUrl, members }: GameCardProps) => {
 
     return (
     <article className="bg-gray-950 rounded-lg p-4 shadow-lg">
-        <div className="flex flex-col gap-4">
-            <div className="rounded-lg overflow-hidden ">
-                <Image src={thumbnailUrl} alt={name} width={320} height={170} />
-            </div>
-            <div className="w-full flex justify-between items-center">
-                <h3 className="text-xl font-bold">{name}</h3>
-                <div className="flex items-center gap-2">
-                    <p className="text-gray-400">{members}</p>
-                    <Image src="/people.svg" alt="members" width={20} height={20} className="filter invert"/>
+        <Link href={`/games/${gameSlug}`}>
+            <div className="flex flex-col gap-4">
+                <div className="rounded-lg overflow-hidden ">
+                    <Image src={thumbnailUrl} alt={name} width={320} height={170} />
+                </div>
+                <div className="w-full flex justify-between items-center">
+                    <h3 className="text-xl font-bold">{name}</h3>
+                    <div className="flex items-center gap-2">
+                        <p className="text-gray-400">{members}</p>
+                        <Image src="/people.svg" alt="members" width={20} height={20} className="filter invert"/>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
         <div className="mt-4 flex gap-4 w-full">
-            <Link href={`/games/${gameSlug}`} className="w-full">
-                <button className="w-full bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
-                    Open
-                </button>
-            </Link>
             <button onClick={(e) => handleJoinClick(e)} className="w-full bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
                 {joined ? "Leave" : "Join"}
             </button>
