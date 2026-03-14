6c9a072a6b173ea4f1ce5f54e6508137794aca69
Anthobad <anthonybadawi100@gmail.com>
2026-03-14 11:54:17 +0200

    Create section in create for builder

--- app/create-strat/page.tsx
+++ app/create-strat/page.tsx
@@ -1,14 +1,21 @@
 'use client'
+import Builder from "@/components/Builder"
 import CreateForm from "@/components/CreateForm"
 
 const page = () => {
   return (
     <main>
-        <article className="mt-12 p-4 border border-gray-300 rounded-2xl w-full gap-4 flex flex-col bg-[#161616] lg:w-1/3 md:w-2/3 mx-auto">
-            <h1 className="text-3xl font-bold">Strategy Builder</h1>
-
+      <div className="px-4 md:px-10 w-full">
+        <article className="mt-12 p-4 border border-gray-300 rounded-2xl w-full gap-4 flex bg-[#161616] mx-auto">
+          <section className="w-2/3">
+            <Builder />
+          </section>
+          <section className="w-1/3">
+            <h1 className="text-3xl font-bold mb-2">Strategy Builder</h1>
             <CreateForm />
+          </section>
         </article>
+      </div>
     </main>
   )
 }
