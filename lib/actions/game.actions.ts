80b083b31d7331574732441440f65c621e834e1b
Anthobad <anthonybadawi100@gmail.com>
2026-03-17 23:52:10 +0200

    game fetching search param functiion and filter

--- lib/actions/game.actions.ts
+++ lib/actions/game.actions.ts
@@ -2,11 +2,12 @@
 import { client } from "@/api/client"
 
 interface GetAllGames {
+    limit: number;
     genre: string | string[];
     topic: string | string[];
 }
 
-export const getAllGames = async ({genre, topic}: GetAllGames) => {
+export const getAllGames = async ({limit, genre, topic}: GetAllGames) => {
     let query = client.from('games').select();
 
     if(genre && topic){
@@ -20,7 +21,6 @@ export const getAllGames = async ({genre, topic}: GetAllGames) => {
         query = query.or(`name.ilike.%${topic}%,description.ilike.%${topic}%`)
     }
 
-
     // if(name){
     //     query.ilike('name', `%${name}%`)
     // }
@@ -30,6 +30,10 @@ export const getAllGames = async ({genre, topic}: GetAllGames) => {
     // if(description){
     //     query.or(`description.ilike.%${description}%`)
     // }
+
+    if(limit !== 0){
+        query = query.limit(limit)
+    }
 
     const {data: games, error} = await query;
 
