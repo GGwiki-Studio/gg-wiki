a8094470712d27b60a65b4f015229cbb4bbe295b
Anthobad <anthonybadawi100@gmail.com>
2026-03-18 02:47:49 +0200

    allowed fetch images from  supa storage

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["picsum.photos"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bublrrtkbcsajpjntctf.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
 };
 
