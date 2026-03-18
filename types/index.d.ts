704238acd4e4032e87d119f356cb45370a30f555
Anthobad <anthonybadawi100@gmail.com>
2026-03-18 12:17:22 +0200

    added common interface strat

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,3 +4,14 @@ interface Game{
   cover_image_url: string;
   member_count: number;
 }
+
+interface Strat{
+  id: string;
+  title: string;
+  thumbnailUrl: string;
+  view_count: number;
+  created_at: string;
+  author: string;
+  gameName: string;
+  mapName: string;
+}
